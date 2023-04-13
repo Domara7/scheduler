6990f0aad1970752317fef872969bbcf7d837e69
Domara Hawil <domara.hawil@hotmail.com>
2023-04-12 21:29:30 -0400

    styled the daylist items

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,30 @@
 import React from "react";
+import "components/DayListItem.scss";
+import classNames from "classnames";
 
 export default function DayListItem(props) {
+  function organizeSpots() {
+    if (props.spots === 0) {
+      return "no spots remaining";
+    }
+    if (props.spots === 1) {
+      return "1 spot remaining";
+    } else {
+      return `${props.spots} spots remaining`;
+    }
+  }
+  let dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": props.spots === 0,
+  });
   return (
-    <li onClick={() => props.setDay(props.name)}>
+    <li
+      onClick={() => props.setDay(props.name)}
+      className={dayClass}
+      data-testid="day"
+    >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{organizeSpots(props.spots)}</h3>
     </li>
   );
 }
