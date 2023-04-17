2e2114fecc5252e8bf956ecc93db7fb7887a1ef2
Domara Hawil <domara.hawil@hotmail.com>
2023-04-17 16:48:41 -0400

    made setDay and setDays function gor application.js and refactored the Daylist in application function to use those functions

--- src/components/Application.js
+++ src/components/Application.js
@@ -47,8 +47,13 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const setDay = (day) => setState({ ...state, day });
+  const setDays = (days) => setState((prev) => ({ ...prev, days }));
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {},
+  });
 
   useEffect(() => {
     const urlDays = `http://localhost:8001/api/days`;
@@ -72,7 +77,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+          <DayList days={state.days} day={state.day} onChange={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
