5289dc10d05689d3746f27aa5e41bb95f11cb7f2
Domara Hawil <domara.hawil@hotmail.com>
2023-04-21 13:43:50 -0400

    created test file for Form and made cases for students

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,7 +1,10 @@
 import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import Form from "components/Appointment/Form";
+
 import { fireEvent } from "@testing-library/react";
+
+import { render, cleanup } from "@testing-library/react";
+
+import Form from "components/Appointment/Form";
 
 afterEach(cleanup);
 
